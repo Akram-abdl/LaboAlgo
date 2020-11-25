60810f29c5dbcd456840a0ba9ac9306ec4585727
IShungite <ishungite@gmail.com>
2020-11-25 14:45:52 +0100

    menu part2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from board import Board
 from constants import WIN_SIZE, FPS, MARGIN, SQUARE_SIZE
 from button import Button
+from paths import imagePath
 
 pygame.init()
 WIN = pygame.display.set_mode((WIN_SIZE[0], WIN_SIZE[1]))
@@ -9,11 +10,11 @@ pygame.display.set_caption("Dofus like")
 
 
 def game():
-    WIN.fill((0, 0, 0))
     run = True
     clock = pygame.time.Clock()
     board = Board()
     while run:
+        WIN.fill((0, 0, 0))
         clock.tick(FPS)
 
         for event in pygame.event.get():
@@ -37,15 +38,45 @@ def game():
         pygame.display.update()
 
 
+def settings():
+    backBtn = Button(f"{imagePath}/menu/back_normal.png", f"{imagePath}/menu/back_hover.png", (700, WIN_SIZE[1] - 100), WIN)
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        WIN.fill((0, 0, 0))
+        clock.tick(FPS)
+
+        mousePos = pygame.mouse.get_pos()
+
+        backBtn.hovered = backBtn.imagerect.collidepoint(mousePos)
+        backBtn.draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if backBtn.hovered:
+                    run = False
+                    break
+
+        pygame.display.update()
+
+
 def main_menu():
-    WIN.fill((0, 0, 0))
-    playBtn = Button("img/play.png", "img/play_hover.png", (700, 200), WIN)
-    optionBtn = Button("img/settings.png", "img/settings.png", (700, 500), WIN)
+    playBtn = Button(f"{imagePath}/menu/start_normal.png", f"{imagePath}/menu/start_hover.png", (700, 200), WIN)
+    settingsBtn = Button(f"{imagePath}/menu/settings_normal.png", f"{imagePath}/menu/settings_hover.png", (700, 500), WIN)
+    clock = pygame.time.Clock()
     while True:
+        WIN.fill((0, 0, 0))
+        clock.tick(FPS)
+
         mousePos = pygame.mouse.get_pos()
 
         playBtn.hovered = playBtn.imagerect.collidepoint(mousePos)
         playBtn.draw()
+        settingsBtn.hovered = settingsBtn.imagerect.collidepoint(mousePos)
+        settingsBtn.draw()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -55,8 +86,8 @@ def main_menu():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if playBtn.hovered:
                     game()
-                elif optionBtn.hovered:
-                    pass
+                elif settingsBtn.hovered:
+                    settings()
 
         pygame.display.update()
 
---
+++ paths.py
@@ -0,0 +1,5 @@
+import os
+
+assetPath = f"{os.getcwd()}{os.sep}assets{os.sep}"
+
+imagePath = f"{assetPath}images{os.sep}"
